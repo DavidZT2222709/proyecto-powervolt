8143f9adb6797a63c843f644fd6d6ed30b6d5ba1
DavidZT2222709 <145131653+DavidZT2222709@users.noreply.github.com>
2025-10-27 11:34:13 -0500

    Merge jeison

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
-<<<<<<< HEAD
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App.jsx";
 import "./index.css";
 
 // 🔹 Importar tus componentes
@@ -36,15 +36,3 @@ createRoot(document.getElementById("root")).render(
     </BrowserRouter>
   </StrictMode>
 );
-=======
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
->>>>>>> jeison
