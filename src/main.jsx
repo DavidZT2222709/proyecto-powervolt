b945bb57ea1b99756e9a2b92a32d55963d100071
DavidZT2222709 <145131653+DavidZT2222709@users.noreply.github.com>
2025-10-28 11:36:24 -0500

    Se raealizó el merge de la rama Jeison al main, se corrigieron los esrrores que se presentaron luego de la unión

--- src/main.jsx
+++ src/main.jsx
@@ -1,38 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 
 // 🔹 Importar tus componentes
-import Login from "./Login.jsx";
-import AdminDashboard from "./components/dashboard/admin/AdminDashboard.jsx";
-import UserDashboard from "./components/dashboard/worker/UserDashboard.jsx";
-import InventoryPanel from "./components/InventoryPanel.jsx";
-import WarrantiesPanel from "./components/dashboard/worker/WarrantiesPanel.jsx";
-import HistoryPanel from "./components/dashboard/admin/HistoryPanel.jsx";
 
 // ⚙️ Crear la raíz
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        {/* 🔹 Página de Login */}
-        <Route path="/" element={<Login />} />
-
-        {/* 🔹 Rutas del Administrador */}
-        <Route path="/admin" element={<AdminDashboard />}>
-          <Route path="historial" element={<HistoryPanel />} />
-          <Route path="garantias" element={<WarrantiesPanel />} />
-        </Route>
-
-        {/* 🔹 Rutas del Usuario / Empleado */}
-        <Route path="/worker" element={<UserDashboard />}>
-          <Route path="inventario" element={<InventoryPanel />} />
-          <Route path="historial" element={<HistoryPanel />} />
-          <Route path="garantias" element={<WarrantiesPanel />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <App />
   </StrictMode>
 );
